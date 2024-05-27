48e740d98cfdd31041e4d7029faa7e4ffb80541f
VladRastortguev <vladrastorguev.ru@gmail.com>
2024-05-27 15:58:29 +0600

    fix sogl

---
+++ src/Component/ContentPerSoglPage/SoglPageNotOk.tsx
@@ -0,0 +1,12 @@
+import { observer } from 'mobx-react-lite';
+import React, { FC } from 'react';
+
+const SoglPageNotOk:FC = () => {
+    return (
+        <div>
+            Not OK
+        </div>
+    );
+};
+
+export default observer(SoglPageNotOk);
